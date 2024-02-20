49529462bdefc489b1f8df5b3a53e33e5a086c18
CyberKraft23 <rijulraman23@gmail.com>
2024-02-20 11:36:46 +0530

    Successfully extracted coefficients from the ML model for future prediction data

--- model.py
+++ model.py
@@ -2,6 +2,11 @@ import nvdlib
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
 
 def cyclicEncode(year_month):
     year, month = int(year_month[:4]), int(year_month[4:6])
@@ -9,44 +14,51 @@ def cyclicEncode(year_month):
     year_angle = 2.0 * np.pi * (year % 100) / 100  
     return [np.sin(month_angle), np.cos(month_angle), np.sin(year_angle), np.cos(year_angle)]
 
-r = nvdlib.searchCVE(keywordSearch= "Exynos",key = "6a7fd5d3-9090-444f-97b3-3dccde149d41", delay = 1)
+def regressor(r):
+    x = []
+    y = []
 
-x = []
-y = []
-for eachCVE in r:
-    try:
-        y.append(eachCVE.v31score)
-        x.append(eachCVE.published[:4]+eachCVE.published[5:7])
-    except:
+    for eachCVE in r:
         try:
-            y.append(eachCVE.v30score)
+            y.append(eachCVE.v31score)
             x.append(eachCVE.published[:4]+eachCVE.published[5:7])
         except:
             try:
-                y.append(eachCVE.v2score)
+                y.append(eachCVE.v30score)
                 x.append(eachCVE.published[:4]+eachCVE.published[5:7])
             except:
-                pass
+                try:
+                    y.append(eachCVE.v2score)
+                    x.append(eachCVE.published[:4]+eachCVE.published[5:7])
+                except:
+                    pass
 
-X_encoded = np.array([cyclicEncode(date) for date in x])
+    X_encoded = np.array([cyclicEncode(date) for date in x])
 
-Y = np.array(y).reshape(-1, 1)
+    Y = np.array(y).reshape(-1, 1)
 
-model = LinearRegression()
-model.fit(X_encoded, Y)
+    model = LinearRegression()
+    model.fit(X_encoded, Y)
 
-future_dates = ['202501', '202502', '202503', '202504', '202505']
-future_X_encoded = np.array([cyclicEncode(date) for date in future_dates])
+    future_dates = ['202403','202404','202405','202406','202407','202408','202409','202410','202411','202412','202501', '202502', '202503', '202504', '202505']
+    future_X_encoded = np.array([cyclicEncode(date) for date in future_dates])
 
-future_y_pred = model.predict(future_X_encoded)
+    future_X_encoded = np.array([cyclicEncode(date) for date in future_dates])
+    future_predictions = model.predict(future_X_encoded)
 
-plt.scatter(x, y, color='blue', label='Original data')
-plt.plot(future_dates, future_y_pred, color='green', label='Future predictions')
+    future_model = LinearRegression()
+    future_model.fit(np.arange(len(future_dates)).reshape(-1, 1), future_predictions)
 
-plt.xlabel('Time')
-plt.ylabel('Y')
-plt.title('Linear Regression with Future Predictions')
+    future_coefficients = future_model.coef_
+    future_intercepts = future_model.intercept_
 
-plt.grid(True)
-plt.legend()
-plt.show()
+    return (future_coefficients, future_intercepts)
+
+
+
+technologies = ["Exynos","Galaxy"]
+
+for technology in technologies:
+    r = nvdlib.searchCVE(keywordSearch= technology,key = api_key, delay = 1)
+    coefficients = regressor(r)
+    print(coefficients)
