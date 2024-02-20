8078533b4e6d543f87717d0d37a9d0c72016e2f1
Pranav Bhat <pranavbhat2004@gmail.com>
2024-02-20 19:22:16 +0530

    Added extra fields to the json body.

--- controller.py
+++ controller.py
@@ -3,7 +3,7 @@ import nvdlib
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from model import regressor, cyclicEncode
+from model import regressor, cyclicEncode, analysis
 import os 
 from dotenv import load_dotenv
 
@@ -22,12 +22,16 @@ def predict():
     for technology in tech_list:
         r = nvdlib.searchCVE(keywordSearch= technology,key = api_key, delay = 1)
         coefficients = regressor(r)
+        attack_vectors_month = analysis(coefficients[2])
+        attack_vectors_year = analysis(coefficients[3])
 
         body = {
             "coefficients": coefficients[0],
             "intercepts": coefficients[1],
             "base-score-month": coefficients[2],
             "base-score-year": coefficients[3],
+            "attack-vectors-month": attack_vectors_month,
+            "attack-vectors-year": attack_vectors_year,
         }
 
         predicted_data.append(body)
